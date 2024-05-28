d06f57d36bcc836a6c3c05e95f179fb655e8a8fb
azzaaass <mobilelegendsazza@gmail.com>
2024-05-28 22:45:46 +0700

    done | user page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('user.index');
 });
+Route::get('/facilities', function () {
+    return view('user.facilities');
+});
+Route::get('/information', function () {
+    return view('user.information');
+});
+Route::get('/doctor', function () {
+    return view('user.doctor');
+});
+Route::get('/pharmacy', function () {
+    return view('user.pharmacy');
+});
+Route::get('/location', function () {
+    return view('user.location');
+});
+Route::get('/profile', function () {
+    return view('user.profile');
+});
+
